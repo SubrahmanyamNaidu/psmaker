e28f81d8b593a84bb4b16258b75cc93fa368306a
SubrahmanyamNaidu <subrahmanyamnaidu.s@gytworkz.com>
2024-12-08 13:47:50 +0530

    step2 extracting topic from topics and getting explanations from openai

--- main.py
+++ main.py
@@ -2,24 +2,60 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers.string import StrOutputParser
 from langchain_openai import ChatOpenAI
 
+import json 
 from dotenv import load_dotenv
 load_dotenv()
 
 
-prompt=ChatPromptTemplate.from_template("""
-     "You are a beginner who has just learned about the topic \"{topic}\". "
-            "Explain it in a simple, clear, and engaging manner as if you're sharing your understanding with a friend who is new to this topic. "
-            "\n\n1. Start by introducing the topic and why it’s important."
-            "\n2. Explain the key concepts in detail with examples from the real world."
-            "\n3. Conclude with a summary and suggest why someone should learn this topic.\n\nTopic: \"{topic}\""
-""")
+# Load the JSON file containing topics
+def load_topics(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
 
-llm=ChatOpenAI(model="gpt-4o")
+def create_prompt_template():
 
-output_parser=StrOutputParser()
+     prompt=ChatPromptTemplate.from_template("""
+          "You are a beginner who has just learned about the topic \"{topic}\". "
+               "Explain it in a simple, clear, and engaging manner as if you're sharing your understanding with a friend who is new to this topic. "
+               "\n\n1. Start by introducing the topic and why it’s important."
+               "\n2. Explain the key concepts in detail with examples from the real world."
+               "\n3. Conclude with a summary and suggest why someone should learn this topic.\n\nTopic: \"{topic}\""
+     """)
 
-chain=prompt|llm|output_parser
+     return prompt
 
-output=chain.invoke({"topic":"html basics"})
+def generate_explanations(topics):
 
-print(output)
+     prompt=create_prompt_template()
+     llm=ChatOpenAI(model="gpt-4o")
+
+     output_parser=StrOutputParser()
+
+     chain=prompt|llm|output_parser
+     topic_number=1
+     explations ={}
+     for topic_entry in topics:
+         topic=topic_entry['topic']
+         explation=chain.invoke({"topic":topic})
+         explations[f'topic-{topic_number}']={topic:explation}
+         topic_number+=1
+
+     return explations
+
+
+# Save the output to a file
+def save_explanations(output_path, explanations):
+    with open(output_path, 'w') as f:
+        json.dump(explanations, f, indent=4)
+
+
+# Main function
+if __name__ == "__main__":
+    # Input JSON file with topics
+    topics_file = "topics.json"
+    output_file = "explanations.json"
+    
+    topics = load_topics(topics_file)
+    explanations = generate_explanations(topics)
+    save_explanations(output_file, explanations)
+    print(f"Explanations saved to {output_file}")
